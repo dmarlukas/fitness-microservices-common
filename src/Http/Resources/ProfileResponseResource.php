d7b491f31ba535a181cb775550e57a82ecc9d931
nathan <nathan@domesticcat.com.au>
2022-08-08 15:51:52 +1000

    add settings links to profile response

--- src/Http/Resources/ProfileResponseResource.php
+++ src/Http/Resources/ProfileResponseResource.php
@@ -30,10 +30,8 @@ class ProfileResponseResource extends JsonResource
         ];
         $userId = $user->id;
         $data['subscription'] = Subscription::fetchSubscriptionArray($userId);
+        $data['settings'] = $this->settingsLinks();
 
-        $data['termsOfUseUrl'] = env('TERMS_OF_USE_LINK', '');
-        $data['privacyPolicyUrl'] = env('PRIVACY_POLICY_LINK', '');
-        $data['faqUrl'] = env('FAQ_LINK', '');
         $data['share']['link'] = 'https://link.templfitness.app/invite-friends';
         $data['share']['title'] = __("Invite Friends");
         $data['share']['shareUrl'] = env('WEBSITE_SHARE_LINK', '');
@@ -41,5 +39,47 @@ class ProfileResponseResource extends JsonResource
         $data['share']['type'] = "shareButton";
 
         return $data;
+    }
+
+    private function settingsLinks(): array
+    {
+        $universalUrlBase = env('UNIVERSAL_BASE_URL', false);
+        return [
+            [
+                "title" => "Shop Apparel",
+                "type" => "callToActionButton",
+                "link" => $universalUrlBase ."shop-apparel"
+            ],
+            [
+                "title" => "Push Notifications",
+                "type" => "callToActionButton",
+                "link" => $universalUrlBase . "push-notifications"
+            ],
+            [
+                "title" => "Terms of Use",
+                "type" => "callToActionButton",
+                "link" => env('TERMS_OF_USE_LINK', '')
+            ],
+            [
+                "title" => "Privacy Policy",
+                "type" => "callToActionButton",
+                "link" => env('PRIVACY_POLICY_LINK', '')
+            ],
+            [
+                "title" => "Frequently Asked Questions",
+                "type" => "callToActionButton",
+                "link" => env('FAQ_LINK', '')
+            ],
+            [
+                "title" => "Contact Us",
+                "type" => "callToActionButton",
+                "link" => $universalUrlBase . "contact-us"
+            ],
+            [
+                "title" => "Log Out",
+                "type" => "callToActionButton",
+                "link" => $universalUrlBase . "log-out"
+            ]
+        ];
     }
 }
