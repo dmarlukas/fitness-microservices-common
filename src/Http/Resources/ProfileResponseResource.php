ca4dad4045054b5473648bc6e919145c0861c54f
Daniel Lukas <daniel.lukas@domesticcat.com.au>
2022-12-01 14:53:25 +1100

    Added enrolledProgram to UserProfileResponse

--- src/Http/Resources/ProfileResponseResource.php
+++ src/Http/Resources/ProfileResponseResource.php
@@ -29,7 +29,8 @@ class ProfileResponseResource extends JsonResource
             "profilePictureUrl" => $user->profile_picture_url,
             "isOnboarded" => boolval($user->is_onboarded),
             "goal" => $user->goal,
-            "targetArea" => $user->targetArea
+            "targetArea" => $user->targetArea,
+            "enrolledProgram" => $user->enrolled_program,
         ];
         $userId = $user->id;
         $data['subscription'] = Subscription::fetchSubscriptionArray($userId);
